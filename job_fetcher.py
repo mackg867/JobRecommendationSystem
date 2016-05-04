8bce18fb2ba0eb898ca028695ec0242ac9994a16
mackg867 <mackenzie.ginithan@gmail.com>
2016-05-03 19:57:07 -0700

    Added files via upload

--- job_fetcher.py
+++ job_fetcher.py
@@ -3,8 +3,7 @@ import json
 from bs4 import BeautifulSoup
 
 def make_indeed_request():
-    """Note: the Indeed API only returns 10
-    responses per search"""
+    """Note: the Indeed API only returns 10 responses per search"""
     return requests.get('http://api.indeed.com/ads/apisearch?publisher=8088628362474696&q=data+-manager+-senior+-big+-director&l=Portland%2C+OR&&radius=25&limit=100&v=2&format=json&sort=date')
     
 def make_dice_request(search_text,page_num):
@@ -12,17 +11,14 @@ def make_dice_request(search_text,page_num):
     keys = {'text': search_text,'page': page_num,'pgcnt': '50','sort': '1','state': 'OR','city': 'Portland','sd': 'd'}
     return requests.get('http://service.dice.com/api/rest/jobsearch/v1/simple.json?',params=keys)
 
-def get_results(response):
-    """Designed to work with Dice JSON reponse"""
+def get_dice_results(response):
     data = json.loads(response.text)
     try:
         return data['resultItemList']
     except:
         return []
     
-def get_job_summary(job_posting):
-    """Designed to work with the Dice JSON 
-    response"""
+def get_dice_job_summary(job_posting):
     try:
         data = requests.get(job_posting['detailUrl'])
     except:
@@ -36,8 +32,3 @@ def get_job_summary(job_posting):
             return summary.replace("\n"," ")
         else:
             return " "
-    
-def get_job_title(job_posting):
-    """Designed to work with the Dice JSON
-    response"""
-    return job_posting['jobTitle']
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import ngrams as ng
 import job_fetcher as jf
-import webbrowser as wb
-import time
-import database_plumbing as db
+from webbrowser import open 
+from time import sleep
+from database_plumbing import DB_Connection 
 
 def get_search_results():
     search_results = []
     for num in ['1','2','3','4']:
         response = jf.make_dice_request('data',num)
-        search_results += jf.get_results(response)
+        search_results += jf.get_dice_results(response)
     return search_results
 
 def get_job_data(search_results,post):
@@ -17,36 +17,28 @@ def get_job_data(search_results,post):
     job_summaries = []
     job_websites = []
     for job in search_results:
-        summary = jf.get_job_summary(job)
+        summary = jf.get_dice_job_summary(job)
         job_websites.append(job['detailUrl'])
         job_titles.append(job['jobTitle'])
         scores.append(ng.score_text(summary,post))
         job_summaries.append(summary)
     return zip(scores,job_titles,job_summaries,job_websites)
-
-def write_data(data):
-    f_handle = open('data.txt','w')
-    for tup in data:
-        for element in tup:
-            f_handle.write('%s,' % element)
-        f_handle.write('\n')
-    f_handle.close()
             
 def open_websites(data):
     for job in data:
         if job[0] > .03:
-            wb.open(job[3])
-            time.sleep(2)
-    
-if __name__ == "__main__":    
+            open(job[3])
+            sleep(2)
+
+if __name__ == "__main__":
     base_post = ng.build_ideal_post()
+    
     results = get_search_results()
     data = get_job_data(results,base_post)
     data = sorted(data,key=lambda tup: tup[0],reverse=True)
     open_websites(data)
     
-    #Write data to database
-    d = db.DB_Connection('jobs.db')
+    d = DB_Connection('jobs.db')
     d.write_data(data)
     d.remove_dups()
     d.close_connection()
